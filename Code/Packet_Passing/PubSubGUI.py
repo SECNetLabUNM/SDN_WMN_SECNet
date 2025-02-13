aa52e6fd6a1cc436a8788ceb263800a94901ce0a
Hieu Quang <hquang@unm.edu>
2025-02-13 01:04:49 -0700

    Prototypes of the client and the GUI, pub sub does work but not effectively. Will revise

--- Code/Packet_Passing/PubSubGUI.py
+++ Code/Packet_Passing/PubSubGUI.py
@@ -5,37 +5,115 @@ import threading
 import time as tm
 import json
 
+def get_local_ip():
+    try:
+        # Create a temporary socket to an external address (e.g., Google's public DNS server)
+        # This doesn't establish a connection, so the target address can be almost anything
+        s = sck.socket(sck.AF_INET, sck.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))  # Google DNS server IP and port
+        local_ip = s.getsockname()[0]  # Get the socket's own address
+        s.close()
+    except Exception as e:
+        print(f"Error: {e}")
+        local_ip = "Unable to determine local IP"
+    return local_ip
+
 ctk.set_appearance_mode("dark")
 ctk.set_default_color_theme("green")
+theme_green = "#2ca373"
 
+client = []
+
+# This is the data frame that is effected by the node switches (sub/unsub is here)
 class DataFrame(ctk.CTkFrame):
-    def __init__(self, master, client):
+    def __init__(self, master):
         super().__init__(master)
-        self.subscribe_state = False
+        self.updated = False
+        self.subscribeXYZ_pressed = False
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_columnconfigure(2, weight=1)
         self.grid_columnconfigure(3, weight=1)
+        self._device_info = []
+        self.state = []
+        self.current_index = 0
         paddingX = 20
         paddingY = 20
 
         self.title = ctk.CTkLabel(master=self, text="ID: NONE", fg_color="gray30", corner_radius=6)
         self.title.grid(row=0, column=0, padx=paddingX, pady=paddingY, sticky="w")
 
-        self.sub_bt = ctk.CTkButton(master=self, text="Subscribe", command=self.subscribe_handler)
-        self.sub_bt.grid(row=0, column=1, padx=0, pady=paddingY, sticky="w")
+        self.subXYZ_bt = ctk.CTkButton(master=self, text="Subscribe XYZ",
+                                       fg_color="gray30", command=self.subscribeXYZ_handler)
+        self.subXYZ_bt.grid(row=0, column=1, padx=0, pady=paddingY, sticky="w")
 
-        self.unsub_bt = ctk.CTkButton(master=self, text="Unsubscribe", command=self.unsubscribe_handler)
-        self.unsub_bt.grid(row=0, column=2, padx=paddingX, pady=paddingY, sticky="w")
+        self.clearXYZ_bt = ctk.CTkButton(master=self, text="Clear", command=self.clearXYZ_handler)
+        self.clearXYZ_bt.grid(row=0, column=2, padx=paddingX, pady=paddingY, sticky="w")
 
-    def update_client_ID(self, new_ID):
-        self.title.configure(text=f"ID: {new_ID}")
+        self.text_box = ctk.CTkFrame(master=self)
+        self.text_box.grid(row=1, column=0, padx=paddingX, columnspan=4, sticky="we")
 
-    def subscribe_handler(self):
-        return 1
+        # Dummy widget to control height
+        self.text_label = ctk.CTkLabel(master=self.text_box, text="", height=35)  # Set the height you need
+        self.text_label.pack(fill="both", expand=True)
 
-    def unsubscribe_handler(self):
-        return 1
+    def contained(self, device_id):
+        for i in self.state:
+            if device_id in i:
+                return True
+        return False
+
+    def update_client_ID(self, device_info):
+        self._device_info = device_info
+        self.updated = True
+        for i, c in enumerate(client):
+            if c[0] == self._device_info[0]:
+                self.current_index = i
+                break
+
+        self.title.configure(text=f"ID: {self._device_info[0]}")
+        self.update_widget_XYZ()
+
+    # Client packet:
+    # Index 0: device ID
+    # Index 1: device neighbors
+    # Index 2: device XYZ
+    # Index 3: index when added, this will be changed on the server side
+    def subscribeXYZ_handler(self):
+        global client
+
+        if not self.subscribeXYZ_pressed:
+            self.subscribeXYZ_pressed = True
+            client[self.current_index][2][0] = True
+        else:
+            self.subscribeXYZ_pressed = False
+            client[self.current_index][2][0] = False
+
+        # Making sure we do not click on an empty frame
+        if len(self._device_info) > 0:
+            self.update_widget_XYZ()
+
+    def update_widget_XYZ(self):
+        device_status = self._device_info[2][0]
+        if device_status:
+            self.subXYZ_bt.configure(fg_color=theme_green)
+            self.text_label.configure(text=f"Subbed {self._device_info[0]}")
+        else:
+            self.subXYZ_bt.configure(fg_color="firebrick1")
+            self.text_label.configure(text=f"Unsub {self._device_info[0]}")
+
+    def return_device_state(self):
+        return self._device_info
+
+    def return_updates(self):
+        if self.updated:
+            self.updated = False
+            return True
+        else:
+            return self.updated
+
+    def clearXYZ_handler(self):
+        self.text_label.configure(text=f"")
 
 # This is the frame class of the switches / nodes themselves
 class SwitchFrame(ctk.CTkScrollableFrame):
@@ -48,11 +126,11 @@ class SwitchFrame(ctk.CTkScrollableFrame):
         self.title.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="ew")
         self.client_btns = []
 
-    def button_action(self, client):
-        self._data_frame.update_client_ID(client)
+    def button_action(self, _client):
+        self._data_frame.update_client_ID(_client)
 
-    def update_client_buttons(self, client):
-        self._client = client
+    def update_client_buttons(self, _client_list):
+        self._client = _client_list
         # Clears old buttons
         for w in self.winfo_children():
             if isinstance(w, ctk.CTkButton) and w != self.title:
@@ -64,7 +142,8 @@ class SwitchFrame(ctk.CTkScrollableFrame):
         # Create new buttons
         index = 0
         for i in self._client:
-            btn = ctk.CTkButton(master=self, text=f"{i}", corner_radius=6, command=lambda i=i: self.button_action(i))
+            btn = ctk.CTkButton(master=self, text=f"{i[0]}", corner_radius=6,
+                                command=lambda i=i: self.button_action(i))
             btn.grid(row=index + 1, column=0, padx=10, pady=(10, 0), sticky="ew")
             self.client_btns.append(btn)
             index += 1
@@ -79,56 +158,91 @@ class PubSubGUI(ctk.CTk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
-        self.client = []
         self.client_btns = []
         self.updated_client_list = False
+        self.updated_data_frame = False
 
-        BAT_server = threading.Thread(target=self.BATMAN_server_handler, args=())
-        BAT_server.start()
-
-        self.data_frame = DataFrame(self, self.client)
+        self.data_frame = DataFrame(self)
         self.data_frame.grid(row=0, column=1, padx=20, pady=20, sticky="nsew")
 
         self.switch_frame = SwitchFrame(self, self.data_frame)
         self.switch_frame.grid(row=0, column=0, padx=20, pady=20, sticky="nsew")
 
-        # This is the updater for all the data. It runs every 1 second
-        self.update_GUI()
+        time.sleep(0.1)
+        BAT_server = threading.Thread(target=self.BATMAN_server_handler, args=())
+        BAT_server.start()
+
+    def overwrite_client_list(self, new_list):
+        global client
+        # if a button from the data frame was pressed, must check some conditions
+        if self.data_frame.return_updates():
+            return 1
+
+        else:
+            client = new_list
 
     def BATMAN_server_handler(self):
+        global client
         # These values are for IPC, the server address is just the device address
         # Changing the port is not recommended but if you do change it, do it on the server side
-        BAT_server_addr = "192.168.1.26"
+        BAT_server_addr = get_local_ip()
         server_port = 1111
         GUI_client_socket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-        holder_client = []
+
         while True:
             try:
                 GUI_client_socket.connect((BAT_server_addr, server_port))
                 while True:
+                    # Receiver
                     data = GUI_client_socket.recv(1024)
-                    if data:
-                        info = json.loads(data.decode('utf-8'))
-                        if holder_client != info:
-                            holder_client = info
-                            if holder_client != self.client:
-                                self.client = holder_client
+                    info = json.loads(data.decode('utf-8'))
+                    if info:
+                        if len(client) == 0:
+                            client = info
+                            self.updated_client_list = True
+                        else:
+                            # If a list sent by controller len != self.client length,
+                            # we know that something has been updated
+                            if len(info) != len(client):
+                                client = info
                                 self.updated_client_list = True
-                                print(self.client)
-                        time.sleep(1)
+                            # If it's the same length, have to check ID's
+                            else:
+                                non_match = False
+                                for index, i_info in enumerate(info):
+                                    if i_info[0] != client[index][0]:
+                                        non_match = True
+                                        break
+
+                                if non_match:
+                                    client = info
+                                    self.updated_client_list = True
+
+                                # This will iterate through the packet and check if the data
+                                # is true, if it is fill in the information
+                                for index, i_info in enumerate(info):
+                                    if i_info[1][0] and client[index][1][0]:
+                                        client[index][1] = i_info[1]
+                                    if i_info[2][0] and client[index][2][0]:
+                                        client[index][2] = i_info[2]
+
+                    if self.updated_client_list:
+                        # Update the buttons if needed
+                        print("updated list")
+                        self.switch_frame.update_client_buttons(client)
+                        self.updated_client_list = False
+
+                    # Sender
+                    json_data = json.dumps(client)
+                    GUI_client_socket.sendall(json_data.encode('utf-8'))
+
+                    time.sleep(1)
 
             except sck.error:
                 print(f"Error: {sck.error}")
                 print(f"Retying to connect to server: {BAT_server_addr}")
                 GUI_client_socket.close()
                 tm.sleep(1)
-
-    # This is for synchronization and updating parts of the GUI
-    def update_GUI(self):
-        if self.updated_client_list:
-            self.switch_frame.update_client_buttons(self.client)
-            self.updated_client_list = False
-        self.after(250, self.update_GUI)
 
 if __name__ == "__main__":
     gui = PubSubGUI()
--- Code/Packet_Passing/pubsub_client13.py
+++ Code/Packet_Passing/pubsub_client13.py
@@ -3,40 +3,95 @@ import threading
 import json
 import random
 import time
+from multiprocessing.connection import Client
 
-server_ADDR = "192.168.1.26"
+
+def get_local_ip():
+    try:
+        # Create a temporary socket to an external address (e.g., Google's public DNS server)
+        # This doesn't establish a connection, so the target address can be almost anything
+        s = sck.socket(sck.AF_INET, sck.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))  # Google DNS server IP and port
+        local_ip = s.getsockname()[0]  # Get the socket's own address
+        s.close()
+    except Exception as e:
+        print(f"Error: {e}")
+        local_ip = "Unable to determine local IP"
+    return local_ip
+
+server_ADDR = get_local_ip()
 server_PORT = 9559
 
-def gen_random_number():
-    return random.randint(0, 999)
+# Index 0: device ID
+# Index 1: device neighbors
+# Index 2: device XYZ
+class ClientHandler:
+    def __init__(self, addr, port):
+        self._ADDR = addr
+        self._Port = port
+        self.device_id = self.gen_random_number()
+        self.device_info = [self.device_id, (False,), (False,)]
 
-def send_info():
-    return 0
+    def gen_random_number(self):
+        return random.randint(0, 999)
 
-def client_handler():
-    device_id = gen_random_number()
-    device_info = {
-        "ID": device_id,
-        "MAC": "00:A0:C9:14:C8:29",
-        "IP": "192.168.1.1",
-        "nMAC": ["00:A0:C9:14:C8:30", "00:A0:C9:14:C8:31", "00:A0:C9:14:C8:32"],
-        "nIP": [1234, 23455, 12323],
-        "LQ": "255"
-    }
+    def gen_random_255(self):
+        return random.randint(1, 255)
 
-    device_xyz = {
-        "ID": device_id,
-        "x": gen_random_number(),
-        "y": gen_random_number(),
-        "z": gen_random_number()
-    }
-    client_socket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
-    client_socket.connect((server_ADDR, server_PORT))
+    def make_device(self, subbed_info, subbed_XYZ):
+        if subbed_info:
+            device_neighbors = {
+                "MAC": "00:A0:C9:14:C8:29",
+                "IP": "192.168.1.1",
+                "nMAC": ["00:A0:C9:14:C8:30", "00:A0:C9:14:C8:31", "00:A0:C9:14:C8:32"],
+                "nIP": ["192.168.1.19", "192.168.1.20", "192.168.1.12"],
+                "LQ": [self.gen_random_255(), self.gen_random_255(), self.gen_random_255()]
+            }
+            self.device_info[1] = (True, device_neighbors)
+        else:
+            self.device_info[1] = (False,)
 
-    while True:
-        json_data = json.dumps(device_id)
-        client_socket.sendall(json_data.encode('utf-8'))
-        # Every 5 seconds we will send to the server
-        time.sleep(5)
+        if subbed_XYZ:
+            device_xyz = {
+                "x": self.gen_random_number(),
+                "y": self.gen_random_number(),
+                "z": self.gen_random_number()
+            }
+            self.device_info[2] = (True, device_xyz)
+        else:
+            self.device_info[2] = (False,)
 
-client_handler()
+    def reply_server(self, client_socket):
+        while True:
+            data = client_socket.recv(1024)
+            if data:
+                conditions = json.loads(data.decode('utf-8'))
+                print(f"Received! {conditions}")
+                condition_neighbors = conditions[0]
+                condition_XYZ = conditions[1]
+                self.make_device(condition_neighbors[1], condition_XYZ[1])
+
+    def connection(self):
+        client_socket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
+        while True:
+            try:
+                client_socket.connect((self._ADDR, self._Port))
+                while True:
+                    server_thread = threading.Thread(target=self.reply_server,
+                                                     args=(client_socket,))
+                    server_thread.start()
+                    json_data = json.dumps(self.device_info)
+                    client_socket.sendall(json_data.encode('utf-8'))
+
+                    # Every 5 seconds we will send to the server
+                    time.sleep(5)
+
+            except sck.error:
+                print(f"Error: {sck.error}")
+                print(f"Retrying to connect to server: {self._ADDR}")
+                client_socket.close()
+                time.sleep(1)
+
+if __name__ == "__main__":
+    client = ClientHandler(server_ADDR, server_PORT)
+    client.connection()
