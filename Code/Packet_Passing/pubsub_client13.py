30db5e602efd5cc26163191a76d36375cd31bf10
Hieu Quang <hquang@unm.edu>
2025-02-17 01:37:30 -0700

    Working prototype of the clients, will need to add data extraction

--- Code/Packet_Passing/pubsub_client13.py
+++ Code/Packet_Passing/pubsub_client13.py
@@ -22,15 +22,19 @@ def get_local_ip():
 server_ADDR = get_local_ip()
 server_PORT = 9559
 
-# Index 0: device ID
-# Index 1: device neighbors
-# Index 2: device XYZ
 class ClientHandler:
     def __init__(self, addr, port):
         self._ADDR = addr
         self._Port = port
         self.device_id = self.gen_random_number()
-        self.device_info = [self.device_id, (False,), (False,)]
+        # This is the main dictionary packet
+        self.device_info = {
+            "ID": self.device_id,
+            "MAC": "00:A0:C9:14:C8:29",
+            "IP": f"192.168.1.{self.gen_random_255()}",
+            "Neighbor_Tuple": [False],
+            "XYZ_Tuple": [False]
+        }
 
     def gen_random_number(self):
         return random.randint(0, 999)
@@ -41,15 +45,13 @@ class ClientHandler:
     def make_device(self, subbed_info, subbed_XYZ):
         if subbed_info:
             device_neighbors = {
-                "MAC": "00:A0:C9:14:C8:29",
-                "IP": "192.168.1.1",
                 "nMAC": ["00:A0:C9:14:C8:30", "00:A0:C9:14:C8:31", "00:A0:C9:14:C8:32"],
                 "nIP": ["192.168.1.19", "192.168.1.20", "192.168.1.12"],
                 "LQ": [self.gen_random_255(), self.gen_random_255(), self.gen_random_255()]
             }
-            self.device_info[1] = (True, device_neighbors)
+            self.device_info["Neighbor_Tuple"] = [True, device_neighbors]
         else:
-            self.device_info[1] = (False,)
+            self.device_info["Neighbor_Tuple"] = [False]
 
         if subbed_XYZ:
             device_xyz = {
@@ -57,40 +59,58 @@ class ClientHandler:
                 "y": self.gen_random_number(),
                 "z": self.gen_random_number()
             }
-            self.device_info[2] = (True, device_xyz)
+            self.device_info["XYZ_Tuple"] = [True, device_xyz]
         else:
-            self.device_info[2] = (False,)
+            self.device_info["XYZ_Tuple"] = [False]
 
     def reply_server(self, client_socket):
-        while True:
-            data = client_socket.recv(1024)
-            if data:
-                conditions = json.loads(data.decode('utf-8'))
-                print(f"Received! {conditions}")
-                condition_neighbors = conditions[0]
-                condition_XYZ = conditions[1]
-                self.make_device(condition_neighbors[1], condition_XYZ[1])
+        try:
+            while True:
+                data = client_socket.recv(1024)
+                if data:
+                    conditions = json.loads(data.decode('utf-8'))
+                    print(f"Received! {conditions}")
+                    condition_neighbors = conditions["Req_Neigh"]
+                    condition_XYZ = conditions["Req_XYZ"]
+                    self.make_device(condition_neighbors, condition_XYZ)
+        except Exception as e:
+            print(f"Error in the reply service{e}")
+            client_socket.close()
 
     def connection(self):
+        first_connection = True
         client_socket = sck.socket(sck.AF_INET, sck.SOCK_STREAM)
         while True:
             try:
                 client_socket.connect((self._ADDR, self._Port))
+                reply_server_thread = threading.Thread(target=self.reply_server,
+                                                       args=(client_socket,))
+                #reply_server_thread.start()
                 while True:
-                    server_thread = threading.Thread(target=self.reply_server,
-                                                     args=(client_socket,))
-                    server_thread.start()
+                    if first_connection:
+                        first_connection = False
+                    else:
+                        data = client_socket.recv(1024)
+                        if data:
+                            conditions = json.loads(data.decode('utf-8'))
+                            print(f"From server! {conditions}")
+                            condition_neighbors = conditions["Req_Neigh"]
+                            condition_XYZ = conditions["Req_XYZ"]
+                            self.make_device(condition_neighbors, condition_XYZ)
+
+                    # TODO: Optimize this to make it only send the TUPLES. The other datas should be static
+                    # TODO: do not troll and try to actually do this
+                    print(f"To server: {self.device_info}")
                     json_data = json.dumps(self.device_info)
                     client_socket.sendall(json_data.encode('utf-8'))
 
-                    # Every 5 seconds we will send to the server
-                    time.sleep(5)
+                    time.sleep(3)
 
             except sck.error:
                 print(f"Error: {sck.error}")
                 print(f"Retrying to connect to server: {self._ADDR}")
                 client_socket.close()
-                time.sleep(1)
+                time.sleep(3)
 
 if __name__ == "__main__":
     client = ClientHandler(server_ADDR, server_PORT)
